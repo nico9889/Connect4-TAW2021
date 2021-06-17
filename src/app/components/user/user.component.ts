d6024e68ff12428b9f07ecb64b3927184c29e557
Nicola <nx9889@gmail.com>
2021-06-17 20:17:03 +0200

    Restored user editing

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {User} from '../../models/user';
 import {UserService} from '../../services/user.service';
@@ -10,6 +10,7 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  @ViewChild('inputElement') inputElement: ElementRef | undefined;
   user: User | undefined;
 
   constructor(private auth: AuthService, private users: UserService, private route: ActivatedRoute, private router: Router) {
@@ -36,13 +37,22 @@ export class UserComponent implements OnInit {
       this.users.edit(this.user._id, {oldPassword, newPassword})
         .subscribe((status) => console.log(status));
     }
-    this.auth.logout();
-    this.router.navigate(['/login']);
   }
 
 
   uploadAvatar(): void {
-
+    const element = this.inputElement?.nativeElement;
+    if (element && element.files?.length === 1) {
+      if (this.user) {
+        this.users.uploadAvatar(this.user._id, element.files.item(0))
+          .subscribe((status) => {
+            console.log(status);
+            if (this.user) {
+              this.users.getUser(this.user._id, true).subscribe();
+            }
+          }, (err) => console.error(err));
+      }
+    }
   }
 
 
@@ -57,5 +67,13 @@ export class UserComponent implements OnInit {
 
   isModerator(): boolean {
     return this.auth.hasRole('MODERATOR');
+  }
+
+  deleteUser(): void {
+    if (this.user) {
+      this.users.delete(this.user?._id).subscribe((_) => {
+        this.router.navigate(['/users']);
+      });
+    }
   }
 }
